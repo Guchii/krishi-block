a5353115a516232fe02ce6bf138a556dee2c7a6e
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2022-11-06 11:03:00 +0530

    setup layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/inter";
+import theme from "../theme";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,13 @@
 import { Box, BoxProps, Grid } from "@chakra-ui/react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
-  const defaultProps : BoxProps = {
-    padding: "30px",
-    fontWeight: "bold"
-  }
-  return (
-    <Grid
-      templateAreas={
-        "'navbar navbar navbar' 'sidebar main main' 'sidebar footer footer'"
-      }
-      minHeight="100vh"
-      gridTemplateRows={"80px auto 100px"}
-      gridTemplateColumns={"300px auto"}
-    >
-      <Box {...defaultProps} gridArea={"navbar"} bg="red.400">
-        navbar
-      </Box>
-      <Box {...defaultProps} gridArea={"sidebar"} bg="red.300">
-        sidebar
-      </Box>
-      <Box {...defaultProps} gridArea={"main"} bg="pink.400">
-        main
-      </Box>
-      <Box {...defaultProps} gridArea={"footer"} bg="blue.400">
-        footer
-      </Box>
-    </Grid>
-  );
+  const defaultProps: BoxProps = {
+    fontWeight: "bold",
+  };
+  return <Box>
+    Shivom vs Gals
+  </Box>;
 }
