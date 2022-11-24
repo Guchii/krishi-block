8ed7f6425ecaf1d4847f5aa9776781f891a3cf55
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2022-11-24 20:41:46 +0530

    auth working fine +jus need state persistence over reloads

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,30 +24,8 @@ const props: ButtonProps = {
 };
 
 const Home: NextPage = () => {
-  const contract = useWeb3Store((state) => state.contract);
-  const [sdmHai, setSDMHai] = useState(false);
-  const [isConnected, connectedAccount] = useWeb3Store(
-    (state) => [state.isConnected, state.connectedAccount],
-    shallow
-  );
+  const [isConnected] = useWeb3Store((state) => [state.isConnected], shallow);
   const router = useRouter();
-  const isSDM = async () => {
-    if (contract && connectedAccount) {
-      const isSDM = await contract.isSDM(connectedAccount);
-      setSDMHai(isSDM);
-    }
-  };
-
-  const checkFunctions = (inp: "1" | "2" | "3" | "4") => {
-    switch (inp) {
-      case "3":
-        return contract?.isSDM;
-      case "2":
-        return contract?.isTehsildar;
-      case "1":
-        return contract?.isLekhpal;
-    }
-  };
 
   const {
     isOpen: adminIsOpen,
@@ -59,41 +37,6 @@ const Home: NextPage = () => {
     onOpen: userOnOpen,
     onClose: userOnClose,
   } = useDisclosure();
-  const userType = useUserStore((state) => state.userType);
-  const setUserType = useUserStore((state) => state.setUserType);
-  const toast = useToast();
-  const checkUserType = async () => {
-    if (isConnected && userType !== undefined && contract) {
-      console.log("Checking user type", userType);
-      const isUserType = await checkFunctions(userType)(connectedAccount);
-      console.log({ isUserType });
-      if (!isUserType) {
-        // set permission mismatch to true
-        // route user to the 403 error route
-        toast({
-          title: `not user of type ${userType}`,
-          description: "redirecting you to the error page",
-          status: "error",
-          duration: 3000,
-          isClosable: true,
-        });
-        router.push("/error?code=403")
-      } else {
-        toast({
-          title: `user of type ${userType}, verified`,
-          description: "You can move to your dashboard now",
-          status: "success",
-          duration: 3000,
-          isClosable: true,
-        });
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (!connectedAccount) setUserType(undefined);
-    checkUserType();
-  }, [userType, connectedAccount]);
 
   return (
     <Grid
