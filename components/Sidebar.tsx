d9b958f40f9fe9d8cc379b5eb143e02fec0d0f57
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2022-11-24 21:24:47 +0530

    update links + cool popover

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,17 @@
-import { Avatar, Button, Stack, Text, VStack } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { FC } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -10,8 +23,11 @@ import useUserStore from "../utils/store";
 
 const Sidebar: FC = () => {
   const router = useRouter();
-  const [ connectedAccount, isConnected ] = useWeb3Store(state => [state.connectedAccount, state.isConnected]);
-  const userType = useUserStore(state => state.userType)
+  const [connectedAccount, isConnected] = useWeb3Store((state) => [
+    state.connectedAccount,
+    state.isConnected,
+  ]);
+  const userType = useUserStore((state) => state.userType);
   return (
     <VStack w="full" pt={8} spacing={4} pos={"sticky"} top={0}>
       {RoleLinks.get(userType || "4")!.map((val, i) => (
@@ -20,7 +36,9 @@ const Sidebar: FC = () => {
             width: "100%",
             marginRight: 32,
             marginBottom:
-              i === RoleLinks.get(userType || "4")!.length - 1 ? "auto" : "none",
+              i === RoleLinks.get(userType || "4")!.length - 1
+                ? "auto"
+                : "none",
           }}
           href={val.href}
           key={i}
@@ -38,35 +56,47 @@ const Sidebar: FC = () => {
           </Button>
         </Link>
       ))}
-      <Stack
-        cursor={"pointer"}
-        position={"fixed"}
-        bottom="0"
-        left={0}
-        gap={4}
-        direction={"row"}
-        p={8}
-        alignItems={"center"}
-        justify={"start"}
-      >
-        <Avatar src="https://source.boringavatars.com/" />
-        <VStack
-          h="full"
-          alignItems={"start"}
-          justifyContent={"center"}
-          lineHeight={0}
-          gap={2}
-        >
-          {isConnected ? (
-            <>
-              <Text>{connectedAccount?.slice(0, 10)}</Text>
-              <Text fontSize={"sm"}>user</Text>
-            </>
-          ) : (
-            <Text>Connect Wallet</Text>
-          )}
-        </VStack>
-      </Stack>
+      <Popover colorScheme={"yellow"}>
+        <PopoverTrigger>
+          <Stack
+            cursor={"pointer"}
+            position={"fixed"}
+            bottom="0"
+            left={0}
+            gap={4}
+            direction={"row"}
+            p={8}
+            alignItems={"center"}
+            justify={"start"}
+          >
+            <Avatar src="https://source.boringavatars.com/" />
+            <VStack
+              h="full"
+              alignItems={"start"}
+              justifyContent={"center"}
+              lineHeight={0}
+              gap={2}
+            >
+              {isConnected ? (
+                <>
+                  <Text>{connectedAccount?.slice(0, 10)}</Text>
+                  <Text fontSize={"sm"}>user</Text>
+                </>
+              ) : (
+                <Text>Connect Wallet</Text>
+              )}
+            </VStack>
+          </Stack>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverArrow />
+          <PopoverCloseButton />
+          <PopoverHeader>Confirmation!</PopoverHeader>
+          <PopoverBody>
+            Are you sure you want to have that milkshake?
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
     </VStack>
   );
 };
