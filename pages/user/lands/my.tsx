8b35fa39976b0b13e2ab2fbed9530b18b1cfafb5
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2022-11-30 18:31:05 +0530

    card modals

--- pages/user/lands/my.tsx
+++ pages/user/lands/my.tsx
@@ -1,25 +1,56 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Link,
+  List,
+  ListIcon,
+  ListItem,
+  MenuIcon,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  StatDownArrow,
+  Text,
+  Tooltip,
+  useColorModeValue,
+  useDisclosure,
+  VStack,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import { useQueryClient } from "@tanstack/react-query";
 import { NextPage } from "next";
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import Card from "../../../components/Card";
+import { useEffect, useState } from "react";
 
+import Card from "../../../components/Card";
+import ViewDetail from "../../../components/ViewLand";
 const Home: NextPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [modalLand, setModalLand] = useState<Partial<Land>>({});
   return (
-    <Wrap spacing={8}>
-      <WrapItem>
-        <Card onSale />
-      </WrapItem>
-      <WrapItem>
-        <Card />
-      </WrapItem>
-      <WrapItem>
-        <Card />
-      </WrapItem>
-      <WrapItem>
-        <Card onSale />
-      </WrapItem>
-    </Wrap>
+    <>
+      <Wrap p={8} spacing={8}>
+        {[...Array(4)].map((_, i) => (
+          <>
+            <WrapItem key={i}>
+              <Card
+                openModal={(land) => {
+                  setModalLand(land);
+                  onOpen();
+                }}
+              />
+            </WrapItem>
+          </>
+        ))}
+      </Wrap>
+      <ViewDetail data={modalLand} isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
-
 
 export default Home;
