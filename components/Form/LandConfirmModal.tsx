d6dd429bcb529ab0e4eee037abff3091b0f0fb91
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2023-01-21 01:31:44 +0530

    fix: increased gas limit

--- components/Form/LandConfirmModal.tsx
+++ components/Form/LandConfirmModal.tsx
@@ -40,7 +40,10 @@ export const LandConfirmModal: FC<{
       data.price,
       data.pid,
       data.survey,
-      data.document
+      data.document,
+      {
+        gasLimit: 6000000
+      }
     );
     console.log('Adding Land ....');
     await txn.wait();
