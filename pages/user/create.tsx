5453ed56dd39f09bdce73d7624f99eb9da8cdd21
XLazer4 <khannarahul446@gmail.com>
2023-01-26 19:04:29 +0530

    added encryption file upload using IPFS

--- pages/user/create.tsx
+++ pages/user/create.tsx
@@ -1,11 +1,46 @@
-import { Box, Button, useDisclosure, VStack } from '@chakra-ui/react';
-import { NextPage } from 'next';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { Box, Button, useDisclosure, VStack } from "@chakra-ui/react";
+import { NextPage } from "next";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { ethers } from "ethers";
+import lighthouse from "@lighthouse-web3/sdk";
 
-import { FormField, UserConfirmModal } from '../../components/Form';
+import { FormField, UserConfirmModal } from "../../components/Form";
 
 const Auth: NextPage = () => {
+  const encryptionSignature = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    const messageRequested = (await lighthouse.getAuthMessage(address)).data
+      .message;
+    const signedMessage = await signer.signMessage(messageRequested);
+    return {
+      signedMessage: signedMessage,
+      publicKey: address,
+    };
+  };
+
+  const progressCallback = (progressData) => {
+    let percentageDone =
+      100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
+    console.log(percentageDone);
+  };
+
+  const deployEncrypted = async (e) => {
+    const sig = await encryptionSignature();
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY as string;
+    console.log(apiKey);
+    const response = await lighthouse.uploadEncrypted(
+      e,
+      sig.publicKey,
+      apiKey,
+      sig.signedMessage,
+      progressCallback
+    );
+    console.log(response.data.Hash);
+  };
+
   const {
     isOpen: confirmIsOpen,
     onOpen: confirmOnOpen,
@@ -23,8 +58,12 @@ const Auth: NextPage = () => {
         <FormField name="name" errorsObj={errors.name} register={register} />
         <FormField name="age" errorsObj={errors.age} register={register} />
         <FormField name="city" errorsObj={errors.city} register={register} />
-        <FormField name="aadhar" errorsObj={errors.aadhar} register={register} />
-        <FormField name="pan" errorsObj={errors.pan} register={register} />
+        <h3>Aadhar</h3>
+        <input onChange={(e) => deployEncrypted(e)} type="file" />
+        <h3>PAN</h3>
+        <input onChange={(e) => deployEncrypted(e)} type="file" />
+        {/* <FormField name="aadhar" errorsObj={errors.aadhar} register={register} /> */}
+        {/* <FormField name="pan" errorsObj={errors.pan} register={register} /> */}
         <FormField
           name="document"
           errorsObj={errors.name}
@@ -32,7 +71,7 @@ const Auth: NextPage = () => {
         />
         <FormField
           name="email"
-          inputProps={{ type: 'email' }}
+          inputProps={{ type: "email" }}
           errorsObj={errors.name}
           register={register}
         />
@@ -42,7 +81,7 @@ const Auth: NextPage = () => {
             setData(formData);
             confirmOnOpen();
           })}
-          colorScheme={'yellow'}
+          colorScheme={"yellow"}
           minW="48"
         >
           Add
